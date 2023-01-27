46e6a7bb6a0e2cae2f404adbdbe74c4aaaec227b
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-01-27 09:51:23 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,39 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from 'components/Section/Section';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    Good: 0,
-    Neutral: 0,
-    Bad: 0,
+export const App = () => {
+  const [Good, setGood] = useState(0);
+  const [Neutral, setNeutral] = useState(0);
+  const [Bad, setBad] = useState(0);
+  const optionsName = ['Good', 'Neutral', 'Bad'];
+
+  const countFeedbacks = type => {
+    switch (type) {
+      case 'Good':
+        setGood(state => state + 1);
+        break;
+
+      case 'Neutral':
+        setNeutral(state => state + 1);
+        break;
+
+      case 'Bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        console.log('Something went wrong');
+        break;
+    }
   };
-  countFeedbacks = feedback => {
-    this.setState(prevState => ({
-      [feedback]: prevState[feedback] + 1,
-    }));
-  };
-  countTotalFeedback = (a, b, c) => {
+
+  const countTotalFeedback = (a, b, c) => {
     return a + b + c;
   };
-  countPositiveFeedbackPercentage = (a, b, c) => {
+  const countPositiveFeedbackPercentage = (a, b, c) => {
     const total = a + b + c;
     if (total === 0) {
       return 0;
@@ -27,47 +42,43 @@ export class App extends Component {
     return `${Math.round((a * 100) / total)}%`;
   };
 
-  render() {
-    const { Good, Neutral, Bad } = this.state;
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 40,
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          backgroundColor: '#dee4e7',
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.countFeedbacks}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          {Good === 0 && Neutral === 0 && Bad === 0 ? (
-            <Notification message="There is no feedback"></Notification>
-          ) : (
-            <Statistics
-              good={Good}
-              neutral={Neutral}
-              bad={Bad}
-              total={this.countTotalFeedback(Good, Neutral, Bad)}
-              positivePercentage={this.countPositiveFeedbackPercentage(
-                Good,
-                Neutral,
-                Bad
-              )}
-            ></Statistics>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        backgroundColor: '#dee4e7',
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={optionsName}
+          onLeaveFeedback={countFeedbacks}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        {Good === 0 && Neutral === 0 && Bad === 0 ? (
+          <Notification message="There is no feedback"></Notification>
+        ) : (
+          <Statistics
+            good={Good}
+            neutral={Neutral}
+            bad={Bad}
+            total={countTotalFeedback(Good, Neutral, Bad)}
+            positivePercentage={countPositiveFeedbackPercentage(
+              Good,
+              Neutral,
+              Bad
+            )}
+          ></Statistics>
+        )}
+      </Section>
+    </div>
+  );
+};
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { DataText } from './Notification.styled';
+
 const Notification = ({ message }) => {
   return (
     <>
